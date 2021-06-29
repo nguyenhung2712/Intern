d9e143823e5177fd2cf4166438131973b6298520
notepower2k1 <nguyenthach617@gmail.com>
2021-06-29 10:06:25 +0700

    Hung beo

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ function RealTime()
     const x = new Date();
     const days = ["Thứ 2","Thứ 3","Thứ 4","Thứ 5","Thứ 6","Thứ 7","Chủ Nhật"]
     const months = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
-    document.getElementById('day').innerHTML = days[x.getDay()] +" ,"+" Ngày " +x.getDate()+ " "+ months[x.getMonth()] +" " + " năm "+ x.getFullYear();
+    document.getElementById('day').innerHTML = days[x.getDay() -1 ] +" ,"+" Ngày " +x.getDate()+ " "+ months[x.getMonth()] +" " + " năm "+ x.getFullYear();
 }
 RealTime();
 
-function addRowHandlers() {
-    document.getElementById('info').innerHTML = "";
-    var myTab = document.getElementById('empTable');
 
-    // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
-    for (i = 1; i < myTab.rows.length; i++) {
+var cells = document.getElementsByTagName('td');
+for(var i = 0; i <= cells.length; i++){
+    if(cells[i])
+   { cells[i].addEventListener('click', function(){
+        alert(this.textContent);
+    });}
+}
 
-        // GET THE CELLS COLLECTION OF THE CURRENT ROW.
-        var objCells = myTab.rows.item(i).cells;
+function UpdateDay()
+{
+    const x = new Date();
+    const days = ["Thứ 2","Thứ 3","Thứ 4","Thứ 5","Thứ 6","Thứ 7","Chủ Nhật"]
+    const months = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
+    let changeday = document.getElementById('datepicker').value;
+    console.log(changeday);
+}
 
-        // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
-        for (var j = 0; j < objCells.length; j++) {
-            info.innerHTML = info.innerHTML + ' ' + objCells.item(j).innerHTML;
-        }
-        info.innerHTML = info.innerHTML + '<br />';     // ADD A BREAK (TAG).
-    }
-    }
