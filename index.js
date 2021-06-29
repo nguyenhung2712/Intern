2a10da60c6399fb5c436aab76b5f03e9d390dc1e
Jaser <nguyenhung12c1@gmail.com>
2021-06-29 11:07:55 +0700

    get

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function RealTime()
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+/* function RealTime()
 {
     const x = new Date();
     const days = ["Thứ 2","Thứ 3","Thứ 4","Thứ 5","Thứ 6","Thứ 7","Chủ Nhật"]
     const months = ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"];
     document.getElementById('day').innerHTML = days[x.getDay()] +" ,"+" Ngày " +x.getDate()+ " "+ months[x.getMonth()] +" " + " năm "+ x.getFullYear();
 }
-RealTime();
+RealTime(); */
 
-function addRowHandlers() {
-    document.getElementById('info').innerHTML = "";
-    var myTab = document.getElementById('empTable');
+$('.form-control').value = 'Add Title';
 
-    // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
-    for (i = 1; i < myTab.rows.length; i++) {
-
-        // GET THE CELLS COLLECTION OF THE CURRENT ROW.
-        var objCells = myTab.rows.item(i).cells;
-
-        // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
-        for (var j = 0; j < objCells.length; j++) {
-            info.innerHTML = info.innerHTML + ' ' + objCells.item(j).innerHTML;
-        }
-        info.innerHTML = info.innerHTML + '<br />';     // ADD A BREAK (TAG).
+var cells = document.getElementsByTagName('td');
+for(var i = 0; i <= cells.length; i++){
+    if(cells[i]) {
+        console.log(cells[i].offsetHeight);
     }
-    }
+}
