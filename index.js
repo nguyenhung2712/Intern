4d41f7bf936e572f16d66a5eb0e3d57cc5ebdc07
truong <truong992001@gmail.com>
2021-07-01 08:56:59 +0700

    button_date

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 
-var today = new Date().toDateString()
-today.slice(0,3)
-lengthToday = today.length
-document.getElementById('day').innerHTML = today.slice(3,lengthToday) ;
-document.getElementById('datepicker').valueAsDate = new Date();
+// var today = new Date().toDateString()
+// today.slice(0,3)
+// lengthToday = today.length
 
-// var today = new Date()
-// var datenow = today.toDateString()
-// document.getElementById("datepicker").value = datenow
+document.getElementById('day').innerHTML = new Date().toDateString() ;
+document.getElementById('datepicker').valueAsDate = new Date()
 
+
+function addOrSubtractDays(dateObj, numDays) {
+   dateObj.setDate(dateObj.getDate() + numDays);
+   return dateObj;
+}
+
+//The next day button
+function NextDay() {
+    let inputValue = document.getElementById('datepicker').valueAsDate;
+    addOrSubtractDays(inputValue,1);
+    document.getElementById('datepicker').valueAsDate = inputValue; 
+    document.getElementById('day').innerHTML = inputValue.toDateString(); 
+}
+
+//The previous day button
+function PreviousDay() {
+    let inputValue = document.getElementById('datepicker').valueAsDate;
+    addOrSubtractDays(inputValue,-1);
+    document.getElementById('datepicker').valueAsDate = inputValue; 
+    document.getElementById('day').innerHTML = inputValue.toDateString();
+}
 
 var cells = document.getElementsByTagName('td');
 for(var i = 0; i <= cells.length; i++){
     if(cells[i])
    { cells[i].addEventListener('click', function(){
-        this.textContent ='test';
+        alert(this.textContent);
     });}
 }
 
 
 function UpdateDay()
 {
-   
     let changeday = document.getElementById('datepicker').value;
     document.getElementById('day').innerHTML = new Date(changeday).toDateString();
     
