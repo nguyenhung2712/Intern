b0687b54d1916081b2e1e9558408bf8113be7eaa
notepower2k1 <nguyenthach617@gmail.com>
2021-07-01 11:44:52 +0700

    test

--- index.js
+++ index.js
@@ -253,11 +253,7 @@ let calenderApp = {
                         const firstNum = Number(i.toString().split('').splice(1, 2).join(''));
                         roomName.innerText = this.roomDetails[firstNum + 1].name;
 
-<<<<<<< HEAD
-                      /*   if(cell.textContent && (!isNaN(this.roomDetails[firstNum + 1].capacity))) {
-=======
                         /* if(cell.textContent && (!isNaN(this.roomDetails[firstNum + 1].capacity))) {
->>>>>>> 2d820cbb7d4351b7462971f170cd04eb61e64f7b
                             this.roomDetails[firstNum + 1].capacity -= 1;
                             this.render();
                         } */
