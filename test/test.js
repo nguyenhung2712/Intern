ea4c08d0da949ce46ea596ff33780c789eb42228
notepower2k1 <nguyenthach617@gmail.com>
2021-07-08 22:23:08 +0700

    update

--- test/test.js
+++ test/test.js
@@ -1,15 +1,57 @@
-var name_email = document.getElementById("name_email");
-var email = document.getElementById("email")
-var photo = document.getElementById("image")
-var data_email = document.querySelector(".data")
-var signinbtn = document.querySelector('.g-signin2');
-console.log(data_email)
 
-function onSignIn(googleUser) {
-var profile = googleUser.getBasicProfile();
-name_email.innerHTML = profile.getName();
-photo.setAttribute('src',profile.getImageUrl());
-name_email.innerHTML = profile.getName();
-email.innerHTML = profile.getEmail();
- // This is null if the 'email' scope is not present.
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      alert('You have signed out !!!')
+    location.assign("/index.html")
+    });
+  }
+const nameData = document.querySelector('#name--data');
+const image = document.querySelector('#img--data');
+const job = document.querySelector('#job--data');
+const place = document.querySelector('#location--data');
+const DoB = document.querySelector('#birthday--data');
+const phone = document.querySelector('#phone--data');
+const fb = document.querySelector('#facebook--data');
+const skype = document.querySelector('#skype--data');
+const about = document.querySelector('#about--data');
+const gender = document.querySelector('#gender--data');
+
+
+const male = document.querySelector('#gender-male')
+const female = document.querySelector('#gender-female')
+const other = document.querySelector('#gender-other')
+
+function CheckGender()
+{
+  if(male.checked==true)
+  gender.value = 'Male'
+  else if (female.checked==true)
+  gender.value ='Female'
+  else if (other.checked==true)
+  gender.value ='Other'
+
+  return gender.value;
+}
+
+function createNewData(id='',image='',place='',DoB='',fb='',skype='',job='',phone='',about='',gender=''){
+    let data = {};
+    data.id = id;
+    data.image = image
+    data.place = place
+    data.DoB = DoB
+    data.fb = fb
+    data.skype = skype
+    data.job = job
+    data.phone = phone
+    data.about =about
+    data.gender = gender
+    return data;
+}
+
+function setToLocalStorage(){
+  var Key = localStorage.getItem('key');
+    var dataIn = createNewData(nameData.value,image.value,place.value,DoB.value,fb.value,skype.value,job.value,phone.value,about.value,CheckGender())
+    localStorage.setItem(Key, JSON.stringify(dataIn))
+   
 }
